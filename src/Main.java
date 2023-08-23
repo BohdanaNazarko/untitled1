c7baf6eabbcc25c396864f3ac6c80dec3e47c4a9
Nazarko Bohdana <nazarko.bodhana@syk.fi>
2023-08-23 15:18:43 +0300

    HELLO

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        int r=6;
+        for (int i=5; r<i; i++){
+            r++;
+            System.out.println(r);
+        }
         System.out.println("Hello world!");
     }
 }
